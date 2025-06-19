b1e82ece5514dcc1ece0b1b93932fcb6504ba978
kymEwhain <ymdwang200@ewhain.net>
2025-06-19 14:12:42 +0900

    Add files via upload

---
+++ spring_server/src/main/java/capston/capston_spring/dto/AccuracySessionDto.java
@@ -0,0 +1,17 @@
+package capston.capston_spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class AccuracySessionDto {
+    private Long songId;
+    private Double score;
+    private String feedback;
+    private String mode;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+}
